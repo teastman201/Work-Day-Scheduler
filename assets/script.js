969edfa80738f8eee359ec903c5bd43e56a66fdb
Tristan Eastman <teastman85@gmail.com>
2020-08-16 17:23:29 -0400

    Changed bootstrap filepath. Added save button. Added bootstrapsave icon to button.

--- assets/script.js
+++ assets/script.js
@@ -9,8 +9,8 @@ var today = $('p').last().text(JSON.stringify(curDay));
 var arrayOfTime = ['9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM'];
 
 // Creates a table within the container element 
-var addTable = $(".container").append("<table>");
-var addClassTable = $('table').addClass("table");
+var table = $('<table>').addClass("table");
+$(".container").append(table);
 // var addRow = $('.table').append('<tr>');
 // var addClassRow = $('tr').addClass('tRow');
 // var addCol = $('.tRow').append('<td></td>');
@@ -19,15 +19,21 @@ var addClassTable = $('table').addClass("table");
 // $('.tRow').append('<input type="text" name="blank"/>');
 // $('.tRow').append('<td></td>');
 
-for (i = 0; i < arrayOfTime.length; i++) {
-    $('.table').append('<tr>');
-    $('tr').addClass('tRow');
-    var td1 = $('<td></td>').text(arrayOfTime[i]);
-    var td2 = $('<input type="text" name="No Text Content"/>');
-    var td3 = $('<td></td>').text('No Text Content');    
-    $('tr').append(td1, td2, td3,);
+    for (i = 0; i < arrayOfTime.length; i++) {
+        //  $('tr').addClass('tRow');
+         var tr = $('<tr></tr>');
+         var td1 = $('<td></td>').text(arrayOfTime[i]);
+         var td2 = $('<input type="text"/>');
+         var td3 = $('<button></button>');
+        $(tr).append(td1, td2, td3);
+        $(table).append(tr);
+        td3.addClass('glyphicon glyphicon-floppy-save');
+    }
+        // $('table').append('<tr>');  }
     // $('.tRow').append('<td></td>');
-}
+// }
+
+// loop;
 
 // function appendText() {
 //     var txt1 = "<p>Text.</p>";               // Create element with HTML 
@@ -35,7 +41,7 @@ for (i = 0; i < arrayOfTime.length; i++) {
 //     var txt3 = document.createElement("p");  // Create with DOM
 //     txt3.innerHTML = "Text.";
 //     $("body").append(txt1, txt2, txt3);      // Append the new elements
-  } 
+  
 
 // arrayOfTime[i];
 
