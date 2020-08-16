d6d6690b8951cea363e060d2902e9eb3d8b82a3b
Tristan Eastman <teastman85@gmail.com>
2020-08-16 13:19:06 -0400

    Added input to table row.

--- assets/script.js
+++ assets/script.js
@@ -4,15 +4,25 @@ var curDay = moment().format('dddd, MMMM Do YYYY');
 // Creates new p element and adds date information to the text area
 var today = $('p').last().text(JSON.stringify(curDay));
 
+// var userInput = 
+
 // Creates a table within the container element 
 var addTable = $(".container").append("<table>");
 var addClassTable = $('table').addClass("table");
 var addRow = $('.table').append('<tr>');
 var addClassRow = $('tr').addClass('tRow');
-var addCol = $('.tRow').append('<td></td>, <td></td>, <td></td>');
+var addCol = $('.tRow').append('<td></td>');
+$('.tRow').append()
+
+$('.tRow').append('<input type="text" name="blank"/>');
+$('.tRow').append('<td></td>');
 
 
-
+// $("select[name=amount]").change(function() {
+//     if ($(this).val() == 'Other') {
+//         $("#custom_price").append('<input type="text" name="amount"/>');
+//     }
+// })
 
 
 
