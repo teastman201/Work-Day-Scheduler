2f7961f6d86123906ac03d80e052e6ef47edad94
Tristan Eastman <teastman85@gmail.com>
2020-08-16 19:33:38 -0400

    Looping on the array to give a single class to each td element.

--- assets/script.js
+++ assets/script.js
@@ -4,25 +4,32 @@ var curDay = moment().format('dddd, MMMM Do YYYY');
 // Creates new p element and adds date information to the text area
 var today = $('p').last().text(JSON.stringify(curDay));
 
-var arrayOfTime = ['9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM'];
+// Array to hold work day time (hours).
+var arrayOfTime = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
 
 // Creates a table within the container element 
 var table = $('<table>').addClass("table");
 $(".container").append(table);
 
-
+// Loop to populate first column with time (hours)
     for (i = 0; i < arrayOfTime.length; i++) {
         var var1 = arrayOfTime[i];
          var tr = $('<tr></tr>');
          var td1 = $('<td></td>').text(arrayOfTime[i]);
          var td2 = $('<input type="text"/>');
+         td2.addClass('input');
          var td3 = $('<button></button>');
+         td3.addClass('glyphicon glyphicon-floppy-save');
         $(tr).append(td1, td2, td3);
         $(table).append(tr);
-        td3.addClass('glyphicon glyphicon-floppy-save');
-        td2.addClass('input');
     }
+    
+    
 
+    for (i = 0; i < arrayOfTime.length; i++) {
+        var tdClass = $('td');
+        tdClass.addClass(arrayOfTime[i]); 
+    }
     
     $('.glyphicon').click(function() {
         var1 = $('td').text();
