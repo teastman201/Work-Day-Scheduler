147543867aadf3c726a95b1a6348b9edfdbb7c1a
Tristan Eastman <teastman85@gmail.com>
2020-08-16 12:59:48 -0400

    Added rows and cells to table.

--- assets/script.js
+++ assets/script.js
@@ -4,12 +4,16 @@ var curDay = moment().format('dddd, MMMM Do YYYY');
 // Creates new p element and adds date information to the text area
 var today = $('p').last().text(JSON.stringify(curDay));
 
+// Creates a table within the container element 
+var addTable = $(".container").append("<table>");
+var addClassTable = $('table').addClass("table");
+var addRow = $('.table').append('<tr>');
+var addClassRow = $('tr').addClass('tRow');
+var addCol = $('.tRow').append('<td></td>, <td></td>, <td></td>');
 
-var createTable = $(".container").append("<table>");
-$('table').addClass("myClass");
-// $( "p" ).addClass( "myClass yourClass" );
 
-// console.log(myClass);
+
+
 
 
 // var timeBlock = $( ".container" ).last();
