cfbfd10c2e2f0164f10f0255c0c278763bb1b508
Tristan Eastman <teastman85@gmail.com>
2020-08-16 12:45:25 -0400

    Added table to container.

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,12 @@ var curDay = moment().format('dddd, MMMM Do YYYY');
 // Creates new p element and adds date information to the text area
 var today = $('p').last().text(JSON.stringify(curDay));
 
+
+var createTable = $(".container").append("<table>");
+$('table').addClass("myClass");
+// $( "p" ).addClass( "myClass yourClass" );
+
+// console.log(myClass);
+
+
+// var timeBlock = $( ".container" ).last();
