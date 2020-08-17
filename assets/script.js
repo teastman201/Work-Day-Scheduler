afdebf6d421062cf6728d5802460392387b56c1d
Tristan Eastman <teastman85@gmail.com>
2020-08-16 20:52:15 -0400

    Changed long-hand save button code. Will refactor after.

--- assets/script.js
+++ assets/script.js
@@ -18,9 +18,11 @@ $(".container").append(table);
          var td1 = $('<td></td>').text(arrayOfTime[i]);
          var td2 = $('<input type="text"/>');
          td2.addClass('input');
-         td1.addClass(var1);
+         
+         //  td1.addClass(var1);
          var td3 = $('<button></button>');
          td3.addClass('glyphicon glyphicon-floppy-save');
+         td3.addClass(var1);
         $(tr).append(td1, td2, td3);
         $(table).append(tr);
     }
@@ -29,19 +31,37 @@ $(".container").append(table);
     //     var tdClass = $('td');
     //     tdClass.addClass(arrayOfTime[i]); 
     // }
-    
-    $('.glyphicon').click(function() {
+    var inputSave = $('.input').val();
+    $('.9AM').click(function(){
+        localStorage.setItem('9AM', JSON.stringify(inputSave));
+    });
+    $('.10AM').click(function(){
+        localStorage.setItem('10AM', JSON.stringify(inputSave));
+    });
+    $('.11AM').click(function(){
+        localStorage.setItem('11AM', JSON.stringify(inputSave));
+    });
+    $('.12PM').click(function(){
+        localStorage.setItem('12PM', JSON.stringify(inputSave));
+    });
+    $('.5PM').click(function(){
+        localStorage.clear();
+    });    
+    console.log(inputSave);
+    // $('.glyphicon').click(function() {
 
 
         // $.each([ 'car', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM' ], function(index, value) {
         //     alert( index + ": " + value );
         // });
         var2 = $(td2).text();
-        var inputSave = $('.input').val();
+        
         localStorage.setItem(var1, JSON.stringify(inputSave));
         // alert('Your data was saved.');
         console.log(var2);
-    });
+    // });
+
+
 
     // add mouseover event
 
