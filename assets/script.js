3926a9af1ba1cf5b18a5f28a35919c2366e6c341
Tristan Eastman <teastman85@gmail.com>
2020-08-16 20:19:28 -0400

    Added classes to each td.

--- assets/script.js
+++ assets/script.js
@@ -18,29 +18,34 @@ $(".container").append(table);
          var td1 = $('<td></td>').text(arrayOfTime[i]);
          var td2 = $('<input type="text"/>');
          td2.addClass('input');
+         td1.addClass(var1);
          var td3 = $('<button></button>');
          td3.addClass('glyphicon glyphicon-floppy-save');
         $(tr).append(td1, td2, td3);
         $(table).append(tr);
     }
-    
-    
-
-    for (i = 0; i < arrayOfTime.length; i++) {
-        var tdClass = $('td');
-        tdClass.addClass(arrayOfTime[i]); 
-    }
+        
+    // for (i = 0; i < arrayOfTime.length; i++) {
+    //     var tdClass = $('td');
+    //     tdClass.addClass(arrayOfTime[i]); 
+    // }
     
     $('.glyphicon').click(function() {
-        var1 = $('td').text();
+
+
+        // $.each([ 'car', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM' ], function(index, value) {
+        //     alert( index + ": " + value );
+        // });
+        var2 = $(td2).text();
         var inputSave = $('.input').val();
         localStorage.setItem(var1, JSON.stringify(inputSave));
         // alert('Your data was saved.');
+        console.log(var2);
     });
 
     // add mouseover event
 
     // add separate button to clear local storage
 
-    console.log(var1);
+    
      
